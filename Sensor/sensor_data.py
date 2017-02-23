bbab5962b8445a8d5313757be0eafadf165edb34
encore-zhou <encorezjm@163.com>
2017-02-23 18:44:13 +0800

    update sensor control

--- Sensor/sensor_data.py
+++ Sensor/sensor_data.py
@@ -8,7 +8,10 @@ serialport = sys.argv[1]
 ser=serial.Serial(serialport, baudrate=115200, timeout=.1, rtscts=0)
 
 while 1:
-	response = ser.read(256)
+	rawdata = ser.read(256)
+	if rawdata[0] == 's' && rawdata[-1] == 'e':
+		print rawdata
+	else
+		print "broken data!"	
 	time.sleep(0.8)
-	print response
 
