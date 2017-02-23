ad74e8640bbd378f9c283cfd10cc101b051d8c01
encore-zhou <encorezjm@163.com>
2017-02-23 18:35:32 +0800

    update sensor control

--- Sensor/sensor_data.py
+++ Sensor/sensor_data.py
@@ -4,9 +4,8 @@ import serial
 import time
 import sys
 
-
-ser=serial.Serial('/dev/ttyUSB0', baudrate=115200, timeout=.1, rtscts=0)
-
+serialport = sys.argv[1]
+ser=serial.Serial(serialport, baudrate=115200, timeout=.1, rtscts=0)
 
 while 1:
 	response = ser.read(256)
