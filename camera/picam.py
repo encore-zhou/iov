02d32ee3c1c8b431cddab5e1b4104eb281062a89
encore-zhou <encorezjm@163.com>
2017-03-08 16:38:09 +0800

    fix bugs with picam.py

--- camera/picam.py
+++ camera/picam.py
@@ -6,7 +6,7 @@ import argparse
 if  __name__ == '__main__':
 	parser = argparse.ArgumentParser(description='capture picture with camera')
 	parser.add_argument('-t' , dest='timeinterval', default=10, type=int, help='time interval to capture a picture')
-	parser.add_argument('dir', help='directory to save the pictures', required=True)
+	parser.add_argument('dir', help='directory to save the pictures')
 	args = parser.parse_args()
 
 	# path = os.path.abspath(os.curdir)
