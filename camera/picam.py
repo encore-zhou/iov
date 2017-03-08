37326019c619947cb8d6d79be415d296bb6e3100
encore-zhou <encorezjm@163.com>
2017-03-08 16:15:36 +0800

    update picam.py

--- camera/picam.py
+++ camera/picam.py
@@ -1,8 +1,13 @@
 import time
 import os
 import sys
+import argparse
 
 if  __name__ == '__main__':
+	parser = argparse.ArgumentParser(description='capture picture with camera')
+	parser.add_argument('-t' , dest='timeinterval', default=10, type=int, help='time interval to capture a picture')
+	args = parser.parse_args()
+
 	path = os.path.abspath(os.curdir)
 	while 1:
 		ticks = time.strftime("%Y%m%d%H%M%S", time.localtime());
@@ -10,4 +15,4 @@ if  __name__ == '__main__':
 		filename = os.path.join(path, 'pictures/'+filename)
 		os.system('raspistill -o '+filename)
 		# do something with the picture
-		time.sleep(10)	
+		time.sleep(args.timeinterval)	
